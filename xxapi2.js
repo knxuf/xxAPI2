f182dbe3a1657b24e70b92914193d63460ec8a00
knxuf <n.stoever+github@gmail.com>
2017-01-24 12:24:08 +0100

    Version 2.041

--- xxapi2.js
+++ xxapi2.js
@@ -43,7 +43,7 @@ $.base64 = {
 }
 
 var xxAPI = {};
-xxAPI.version = "2.040";
+xxAPI.version = "2.041";
 xxAPI.functions = {};
 var performance = window.performance || $ // make performance.now() work in any case
 xxAPI.events = {
